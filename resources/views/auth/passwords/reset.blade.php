96e733492624adc834792a57d541ff0cd6a90fd6
shai <126607972+sSquishy@users.noreply.github.com>
2026-01-02 03:38:14 +0800

    Update reset password blade ui

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,76 +5,92 @@
 @section('content')
 
 
-    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-        {!! csrf_field() !!}
 
-        <div class="container">
-            <div class="row">
+<style>
+    .login-container {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        z-index: 1000;
+        padding: 12px;
+        box-sizing: border-box;
+    }
+    @media (max-width: 767.98px) {
+        .login-container {
+            position: static;
+            transform: none;
+            padding-top: 24px;
+            padding-bottom: 24px;
+        }
+    }
+    .box.login-box {
+        border-radius: 32px;
+        overflow: hidden;
+        background: #ffffff !important;
+    }
+    .box-header .box-title {
+        width: 100%;
+        text-align: center;
+    }
+</style>
 
-                <div class="col-md-6 col-md-offset-3">
+<div class="login-bg" aria-hidden="true" style="position:fixed;inset:0;z-index:800;background-image:url('{{ asset('custom/img/loginbg.jpg') }}');background-size:cover;background-position:center center;background-repeat:no-repeat;pointer-events:none;"></div>
 
-                    <div class="box login-box" style="width: 100%">
-                        <div class="box-header with-border">
-                            <h2 class="box-title"> {{ trans('auth/general.reset_password')  }}</h2>
-                        </div>
-
-
-                        <div class="login-box-body">
-                            <div class="row">
-
-                                <!-- Notifications -->
-                                @include('notifications')
+<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+    {!! csrf_field() !!}
+    <div class="container login-container">
+        <div class="row">
+            <div class="col-md-4 ">
+                <div class="box login-box">
+                    <div class="box-header with-border">
+                        <h2 class="box-title"> {{ trans('auth/general.reset_password')  }}</h2>
+                    </div>
 
 
 
-                                    <input type="hidden" name="token" value="{{ $token }}">
-
-                                    <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                                        <label class="col-md-4 control-label"><x-icon type="user" /> {{ trans('admin/users/table.username')  }}</label>
-
-                                        <div class="col-md-6">
-                                            <input type="text" class="form-control" name="username" value="{{ old('username', $username) }}">
-                                            {!! $errors->first('username', '<span class="alert-msg"><i class="fas fa-times"></i> :message</span>') !!}
-
+                    <div class="login-box-body">
+                        <div class="row">
+                            <!-- Notifications -->
+                            @include('notifications')
+                            <input type="hidden" name="token" value="{{ $token }}">
+                            <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                                <label for="username">
+                                    <x-icon type="user" /> {{ trans('admin/users/table.username')  }}
+                                </label>
+                                <input type="text" class="form-control" name="username" value="{{ old('username', $username) }}">
+                                {!! $errors->first('username', '<span class="alert-msg"><i class="fas fa-times"></i> :message</span>') !!}
                             </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label" for="password">
-                                <x-icon type="password" />
-                                {{ trans('admin/users/table.password')  }}
-                            </label>
-
-                            <div class="col-md-6">
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password">
+                                    <x-icon type="password" />
+                                    {{ trans('admin/users/table.password')  }}
+                                </label>
                                 <input type="password" class="form-control" name="password" aria-label="password">
                                 {!! $errors->first('password', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
                             </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label" for="password_confirmation">
-                                <x-icon type="password" />
-                                {{ trans('admin/users/table.password_confirm')  }}</label>
-                            <div class="col-md-6">
+                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                                <label for="password_confirmation">
+                                    <x-icon type="password" />
+                                    {{ trans('admin/users/table.password_confirm')  }}</label>
                                 <input type="password" class="form-control" name="password_confirmation" aria-label="password_confirmation">
                                 {!! $errors->first('password_confirmation', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
-
                             </div>
                         </div>
-
-
-                            </div>
-                        </div>
-                        <div class="box-footer">
-                            <button type="submit" class="btn btn-lg btn-primary btn-block">
-                                {{ trans('auth/general.reset_password')  }}
-                            </button>
-                        </div>
-
+                    </div>
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-lg btn-primary btn-block">
+                            {{ trans('auth/general.reset_password')  }}
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </form>
 
 @stop
